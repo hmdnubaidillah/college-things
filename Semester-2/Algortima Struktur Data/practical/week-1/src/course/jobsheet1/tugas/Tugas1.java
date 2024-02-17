8e875a740f626826f35075647a455395cbda361f
Nerots <daniuyan71@gmail.com>
2024-02-17 21:26:08 +0700

    fix: tugas1

--- Semester-2/Algortima Struktur Data/practical/week-1/src/course/jobsheet1/tugas/Tugas1.java
+++ Semester-2/Algortima Struktur Data/practical/week-1/src/course/jobsheet1/tugas/Tugas1.java
@@ -5,45 +5,36 @@ import java.util.*;
 public class Tugas1 {
     public static void main(String[] args) {
 
-        String plateCode[] = { "A", "B", "D", "E", "F", "G", "H", "L", "N", "T" };
-        String cityName[][] = {
-                { "BANTEN" },
-                { "JAKARTA" },
-                { "BANDUNG" },
-                { "CIREBON" },
-                { "BOGOR" },
-                { "PEKALONGAN" },
-                { "SEMARANG" },
-                { "SURABYA" },
-                { "MALANG" },
-                { "TEGAL" } };
+        char plateCode[] = { 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'L', 'N', 'T' };
+        char cityName[][] = {
+                { 'B', 'A', 'N', 'T', 'E', 'N' },
+                { 'J', 'A', 'K', 'A', 'R', 'T', 'A' },
+                { 'B', 'A', 'N', 'D', 'U', 'N', 'G' },
+                { 'C', 'I', 'R', 'E', 'B', 'O', 'N', },
+                { 'B', 'O', 'G', 'O', 'R' },
+                { 'P', 'E', 'K', 'A', 'L', 'O', 'N', 'G', 'A', 'N' },
+                { 'S', 'E', 'M', 'A', 'R', 'A', 'N', 'G' },
+                { 'S', 'U', 'R', 'A', 'B', 'A', 'Y', 'A' },
+                { 'M', 'A', 'L', 'A', 'N', 'G' },
+                { 'T', 'E', 'G', 'A', 'L' } };
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Input plate : ");
         String input = sc.nextLine();
-
-        // get first digit of input
-        String firstDigit = String.valueOf(input.charAt(0));
-
-        // get arr index
-        int arrIndex = -1;
-        for (int i = 0; i < plateCode.length; i++) {
-
-            if (plateCode[i].equals(firstDigit.toUpperCase())) {
-                arrIndex = i;
-                break;
-            }
-        }
-
         String city = "";
 
         // print if input equals to city name
-        for (int i = 0; i < cityName.length;) {
-            city += cityName[arrIndex][0];
-            break;
+        for (int i = 0; i < cityName.length; i++) {
+            if (plateCode[i] == input.toUpperCase().charAt(0)) {
+                for (int j = 0; j < cityName[i].length; j++) {
+                    city += cityName[i][j];
+                }
+            } else {
+                System.out.println("City not found");
+                break;
+            }
         }
-
         System.out.println(city);
     }
 }
